6b549cd89e03be2fafa4ce4378e70538744b9aa3
Shengqiang Li <49022799+Shengqiang-Li@users.noreply.github.com>
2021-12-11 18:53:34 +0800

    Add files via upload

---
+++ linear_attention.py
@@ -0,0 +1,70 @@
+
+import torch
+
+from torch import nn
+
+class MultiHeadedLinerAttention(nn.Module):
+    """Multi-Head Linear Attention layer.
+
+    Args:
+        n_head (int): The number of heads.
+        n_feat (int): The number of features.
+        dropout_rate (float): Dropout rate.
+
+    """
+
+    def __init__(self, n_head, n_feat, dropout_rate):
+        """Construct an MultiHeadedAttention object."""
+        super(MultiHeadedLinerAttention, self).__init__()
+        assert n_feat % n_head == 0
+        # We assume d_v always equals d_k
+        self.d_k = n_feat // n_head
+        self.h = n_head
+        self.linear_q = nn.Linear(n_feat, n_feat)
+        self.linear_k = nn.Linear(n_feat, n_feat)
+        self.linear_v = nn.Linear(n_feat, n_feat)
+        self.linear_out = nn.Linear(n_feat, n_feat)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout_rate)
+
+
+    def forward(self, query, key, value, mask=None):
+        """Compute scaled dot product attention.
+
+        Args:
+            query (torch.Tensor): Query tensor (#batch, time1, size).
+            key (torch.Tensor): Key tensor (#batch, time2, size).
+            value (torch.Tensor): Value tensor (#batch, time2, size).
+            mask (torch.Tensor): Mask tensor (#batch, 1, time2) or
+                (#batch, time1, time2).
+
+        Returns:
+            torch.Tensor: Output tensor (#batch, time1, d_model).
+
+        """
+
+        n_batch = query.size(0)
+
+        q = self.linear_q(query).view(n_batch, -1, self.h, self.d_k)
+        k = self.linear_k(key).view(n_batch, -1, self.h, self.d_k)
+        v = self.linear_v(value).view(n_batch, -1, self.h, self.d_k)
+        q = q.transpose(1, 2)  # (batch, head, time1, d_k)
+        k = k.transpose(1, 2)  # (batch, head, time2, d_k)
+        v = v.transpose(1, 2)  # (batch, head, time2, d_k)
+
+        dim = v.size(-1)   # refers to d_k
+        (q, k) = map(lambda x: x * (dim ** -0.25), (q, k))
+
+        q = q.softmax(dim=-1)
+        k = k.softmax(dim=-2)
+
+        context_einsum_eq = 'bhnd,bhne->bhde'
+        context = torch.einsum(context_einsum_eq, k, v)
+
+        attn_einsum_eq = 'bhnd,bhde->bhne'
+        x = torch.einsum(attn_einsum_eq, q, context)
+        x = (
+            x.transpose(1, 2).contiguous().view(n_batch, -1, self.h * self.d_k)
+        )  # (batch, time1, d_model)
+
+        return self.linear_out(x)   # (batch, time1, d_model)
